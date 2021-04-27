d3f1c54e6ab1ab13ff1eed46d18965d0fc7ae1da
Egor Ushakov <eushakov@gmail.com>
2021-04-27 09:43:26 +0300

    Add service for generating document schemas

---
+++ src/Service/DocumentSchemaService.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Annotation\DocumentField;
+use App\DocumentType\IDocument;
+use Doctrine\Common\Annotations\AnnotationReader;
+
+final class DocumentSchemaService
+{
+    private iterable $documentTypes;
+
+    public function __construct(iterable $documentTypes)
+    {
+        $this->documentTypes = $documentTypes;
+    }
+
+    public function __invoke(): array
+    {
+        $result = [];
+        /** @var IDocument $documentType */
+        foreach ($this->documentTypes as $documentType) {
+            $docDefs = [];
+            $fieldDefs = [];
+            $reader = new AnnotationReader();
+            $class = new \ReflectionClass($documentType);
+            $properties = $class->getProperties();
+            foreach ($properties as $property) {
+                $annotation = $reader->getPropertyAnnotation(
+                    $property,
+                    DocumentField::class
+                );
+                $fieldDefs[$property->getName()] = $annotation;
+            }
+            $docDefs['name'] = $documentType->getName();
+            $docDefs['discriminator'] = $documentType->getDiscriminator();
+            $docDefs['fields'] = $fieldDefs;
+            $result[] = $docDefs;
+        }
+
+        return $result;
+    }
+
+}
